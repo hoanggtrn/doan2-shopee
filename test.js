03cad1dd10f1294abfcb2ec97b319ff0cf4ac5b3
hoanggtrn <hoangboivl6969@gmail.com>
2023-07-25 00:18:40 +0700

    v0.0.3
    thêm tìm kiếm theo tên

--- test.js
+++ test.js
@@ -1,7 +1,32 @@
-function showProductDetails(productName) {
-    // Đây là hàm để hiển thị thông tin chi tiết sản phẩm dựa vào tên sản phẩm được chọn.
-    // Trong ví dụ này, chúng ta sẽ sử dụng một đoạn văn bản đơn giản.
-    let productDetailsDiv = document.getElementById('productDetails');
-    productDetailsDiv.innerHTML = `<h2>${productName}</h2>
-                                    <p>Thông tin chi tiết về ${productName}</p>`;
+function removeVietnameseDiacritics(text) {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+function handleSearchInput() {
+    let input = document.getElementById('searchInput');
+    let searchHistory = document.querySelector('.search-history');
+    let noResultsMessage = document.querySelector('.no-results-message');
+
+    input.value = removeVietnameseDiacritics(input.value); // Loại bỏ dấu từ ô nhập liệu
+    searchHistory.innerHTML = input.value; // Hiển thị nội dung của ô nhập liệu vào div "search-history"
+    noResultsMessage.style.display = 'none'; // Ẩn thông báo "Không tìm thấy sản phẩm"
+}
+
+function searchProduct() {
+    let input = document.getElementById('searchInput');
+    let filter = input.value.toUpperCase();
+    let productItems = document.querySelectorAll('.home-product-item');
+    let noResultsMessage = document.querySelector('.no-results-message');
+
+    for (let i = 0; i < productItems.length; i++) {
+        let productName = productItems[i].querySelector('.home-product-item-name').innerText;
+        productName = removeVietnameseDiacritics(productName); // Loại bỏ dấu từ tên sản phẩm
+        if (productName.toUpperCase().indexOf(filter) > -1) {
+            productItems[i].style.display = ''; // Hiển thị sản phẩm phù hợp với tìm kiếm
+            noResultsMessage.style.display = 'none'; // Ẩn thông báo "Không tìm thấy sản phẩm"
+        } else {
+            productItems[i].style.display = 'none'; // Ẩn sản phẩm không phù hợp
+            noResultsMessage.style.display = ''; // Hiển thị thông báo "Không tìm thấy sản phẩm"
+        }
+    }
 }
